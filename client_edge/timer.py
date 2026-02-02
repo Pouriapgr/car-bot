40396fb42f6189207a02f160f030677caa93683a
pouriapgr <pouriainfosarmadi@gmail.com>
2026-02-02 09:55:46 +0330

    Added timer.py for calculating idle timeout.

---
+++ client_edge/timer.py
@@ -0,0 +1,22 @@
+import asyncio
+import config
+from states import BotState
+
+class IdleTimer:
+    def __init__(self, bus):
+        self.bus = bus
+        self.timer_task = None
+
+    async def on_state_change(self, state):
+        if self.timer_task:
+            self.timer_task.cancel()
+
+        if state == BotState.IDLE:
+            self.timer_task = asyncio.create_task(self.countdown())
+
+    async def countdown(self):
+        try:
+            await asyncio.sleep(config.TIMER_COUNTER) 
+            await self.bus.publish("IDLE_TIMEOUT")
+        except asyncio.CancelledError:
+            print("TIMER: Reset")
