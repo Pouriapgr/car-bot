26ca9aa086d8d23996cf833705555a48dcde8588
pouriapgr <pouriainfosarmadi@gmail.com>
2026-02-07 15:43:13 +0330

    Added simple tester for reasoning

---
+++ server_core/testers/reasoning_tester.py
@@ -0,0 +1,16 @@
+import asyncio
+from server_core.services.reasoning import ReasoningModel
+
+async def test():
+
+    llm = ReasoningModel()
+
+    while True:
+        prompt = input()
+        txt = await llm.get_response(prompt)
+
+        print(txt)
+    return False
+
+if __name__ == "__main__":
+    print(asyncio.run(test()))
