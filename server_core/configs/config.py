7e439d18cce33b680646d9f449548473eff595f9
pouriapgr <pouriainfosarmadi@gmail.com>
2026-02-07 15:13:52 +0330

    Dont know changed what. Too tired to dif

--- server_core/configs/config.py
+++ server_core/configs/config.py
@@ -4,13 +4,13 @@ import os
 
 class ModelsConfig:
    
-    REASONING_MODEL_PATH = os.path.join("models", "llama-3-8b-instruct.Q6_K.gguf")
+    REASONING_MODEL_PATH = os.path.join("server_core/models", "qwen2.5-7b-instruct-q6_k-00001-of-00002.gguf")
     REASONING_MODEL_CTX = 4096
     REASONING_MODEL_MAX_TOKENS = 128
 
-    STT_MODEL_PATH = os.path.join("models", "faster-whisper-large-v3")
+    STT_MODEL_PATH = os.path.join("server_core/models", "faster-whisper-large-v3")
     SST_BEAM_SIZE = 4
     STT_LANGUAGE = "fa"
 
     PIPER_PATH = os.path.join("piper", "piper.exe") 
-    TSS_MODEL_PATH = os.path.join("models", " fa_IR-amir-medium.onnx")
+    TSS_MODEL_PATH = os.path.join("server_core/models", " fa_IR-amir-medium.onnx")
